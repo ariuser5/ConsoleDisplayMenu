0b9b35361e3288b0c310d9f4dfd44e326d05f18e
Darius_EC <dariusciuverca@gmail.com>
2019-08-09 00:59:57 +0300

    Created testing project.

---
+++ CDM_run/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleDisplayMenu;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CDM_run
+{
+	internal class Program
+	{
+
+		private static string currentPage = "??";
+
+		private static void Main(string[] args) {
+
+			ConsoleMenu.Render(currentPage);
+			Console.ReadKey();
+
+		}
+	}
+}

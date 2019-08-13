c6f6c8c6ac20ec18da9a30cc23004cab47641264
Darius_EC <dariusciuverca@gmail.com>
2019-08-14 01:34:53 +0300

    Impl. Runtime compile methods.

---
+++ RuntimeCompile/Script.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.CSharp;
+using System;
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RuntimeCompile
+{
+	public class Script
+	{
+
+		private static Assembly BuildAssembly(string code) {
+
+			CSharpCodeProvider provider = new CSharpCodeProvider();
+			CompilerParameters compilerparams = new CompilerParameters {
+				GenerateExecutable = false,
+				GenerateInMemory = true
+			};
+			CompilerResults results =
+			   provider.CompileAssemblyFromSource(compilerparams, code);
+
+			if(results.Errors.HasErrors) {
+				StringBuilder errors = new StringBuilder("Compiler Errors :\r\n");
+				foreach(CompilerError error in results.Errors) {
+					errors.AppendFormat("Line {0},{1}\t: {2}\n",
+						   error.Line, error.Column, error.ErrorText);
+				}
+				throw new Exception(errors.ToString());
+			} else {
+				return results.CompiledAssembly;
+			}
+		}
+
+		public static object Execute(
+			string source,
+			string @namespace = "",
+			string className = "MyClass",
+			string methodName = "Main",
+			bool isStatic = true,
+			params object[] args) {
+
+			object retVal = null;
+			string code = File.ReadAllText(source);
+			Assembly asm = BuildAssembly(code);
+			object instance = null;
+			Type type = null;
+			string typeName = @namespace != string.Empty ? @namespace + className : className;
+
+			if(isStatic) {
+				type = asm.GetType(typeName);
+			} else {
+				instance = asm.CreateInstance(typeName);
+				type = instance.GetType();
+			}
+
+			MethodInfo method = type.GetMethod(methodName);
+			retVal = method.Invoke(instance, args);
+			return retVal;
+		}
+
+	}
+}

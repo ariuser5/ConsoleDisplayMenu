fbd62cf4e9792ef0722abcc78dda76c25eb11cca
Darius_EC <dariusciuverca@gmail.com>
2019-08-14 19:25:18 +0300

    Created Unit test project.

---
+++ UnitTests/ConsoleDisplayMenuTest.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class ConsoleDisplayMenuTest
+	{
+		[TestMethod]
+		public void JsonObject_ReadMetaComponents() {
+
+			var myJson = "Label2: ${Sample\\Methods.vb>Methods.GetThree(#{preset0})} combined value here";
+			var result = ConsoleDisplayMenu.JsonObject.ReadMetaComponents(myJson);
+
+			Assert.IsTrue(result.Count() == 3);
+
+		}
+	}
+}
---
+++ UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e240f760-6e41-4cc0-9e42-0118596e4398")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

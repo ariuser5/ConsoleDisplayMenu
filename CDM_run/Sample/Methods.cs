78979d25c6f69ace2b429d40b73b95f89320ad52
Darius_EC <dariusciuverca@gmail.com>
2019-08-14 19:23:39 +0300

    Added json sample files.

---
+++ CDM_run/Sample/Methods.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Methods
+{
+	public static void MainMenu() {
+		Console.WriteLine("executing MainMenu from Methods class");
+	}
+
+	public static int GetThree() => 3;
+
+}

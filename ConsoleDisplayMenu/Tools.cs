85728192910dfda678b6411011383f87b776d2f9
Darius_EC <dariusciuverca@gmail.com>
2019-08-21 21:03:37 +0300

    Impl. in Tools: -IEnumerable<string> SplitOnce(string, string); -IEnumerable<string> SplitOnce(string, char); -string GetUniqueName(IEnumerable<string>, [string], [string]).

--- ConsoleDisplayMenu/Tools.cs
+++ ConsoleDisplayMenu/Tools.cs
@@ -10,25 +10,90 @@ namespace ConsoleDisplayMenu
 	{
 
 		public static string ReadBetween(string source, char begin, char end) {
-			var builder = string.Empty;
-			var depth = 0;
+			string accumulate = null;
+			int depth = 0;
 
 			foreach(char ch in source) {
 
 				if(ch == begin) {
 					depth++;
-					if(depth == 1) continue;
+					if(depth == 1) {
+						accumulate = string.Empty;
+						continue;
+					}
 
 				} else if(ch == end) {
-					depth--;
-					if(depth == 0) return builder;
 
-				} else {
-					if(depth > 0) builder += ch;
+					if(depth > 0) {
+						depth--;
+
+						if(depth == 0) return accumulate;
+					}
+
 				}
+
+				if(depth > 0) accumulate += ch;
 			}
 
 			return null;
+		}
+
+
+		public static IEnumerable<String> SplitOnce(string source, string delimiter) {
+			string[] rets = { string.Empty, string.Empty };
+			int index = 0;
+			int i = 0;
+			int j = 0;
+
+			while(i < source.Length - delimiter.Length) {
+				if(index == 0 && source[i] == delimiter[j]) {
+
+					while(j < delimiter.Length) {
+						if(source[i + j] != delimiter[j]) break;
+
+						j++;
+					}
+
+					if(j == delimiter.Length) {
+						index = 1;
+						i = i + j;
+					}
+
+					j = 0;
+
+				}
+
+				rets[index] += source[i];
+				i++;
+			}
+
+			return rets.Distinct();
+		}
+
+		public static IEnumerable<string> SplitOnce(string source, char delimiter) {
+			return SplitOnce(source, delimiter);
+		}
+
+		public static string GetUniqueName(
+			IEnumerable<string> refList,
+			string preferedName = null,
+			string defaultName = "Unnamed") {
+
+			var current = string.Empty;
+			var count = 1;
+
+			if(string.IsNullOrEmpty(preferedName)) preferedName = defaultName;
+
+			preferedName = defaultName;
+			current = defaultName;
+
+			while(true) {
+				if(!refList.Contains(current)) return current;
+
+				current = string.Format("{0}({1})", preferedName, count);
+				count++;
+			}
+
 		}
 
 	}

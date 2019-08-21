0eae8f68d22ec2ea7f01a25b66ae9c6a11681a6e
Darius_EC <dariusciuverca@gmail.com>
2019-08-21 19:44:19 +0300

    Added Tools class

---
+++ ConsoleDisplayMenu/Tools.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleDisplayMenu
+{
+	internal class Tools
+	{
+
+		public static string ReadBetween(string source, char begin, char end) {
+			var builder = string.Empty;
+			var depth = 0;
+
+			foreach(char ch in source) {
+
+				if(ch == begin) {
+					depth++;
+					if(depth == 1) continue;
+
+				} else if(ch == end) {
+					depth--;
+					if(depth == 0) return builder;
+
+				} else {
+					if(depth > 0) builder += ch;
+				}
+			}
+
+			return null;
+		}
+
+	}
+}
